b0824680ac0ff04ed54d4d7b7bbdbc6597e6f7cf
halil37 <hllibrahimsartitas@gmail.com>
2024-04-01 17:44:01 +0300

    Linear search project created.

---
+++ src/Algorithm.Search.Linear/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Algorithm.Search.Linear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
