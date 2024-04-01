7ccf9b0a124056893cf218daab61168329062068
halil37 <hllibrahimsartitas@gmail.com>
2024-04-01 17:49:37 +0300

    linear search method added in program.cs

--- src/Algorithm.Search.Linear/Program.cs
+++ src/Algorithm.Search.Linear/Program.cs
@@ -6,7 +6,21 @@ namespace Algorithm.Search.Linear
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arr = { 1, 2, 3 };
+            int target = 3;
+            int num = LinearSearch(arr,target);
+            if(num>0) Console.WriteLine($"Target: {num}");
+
         }
+        static int LinearSearch(int[] arr,int target)
+        {
+            foreach (var item in arr)
+            {
+                if (item == target) return item;
+            }
+
+            return -1;
+        }
+
     }
 }
