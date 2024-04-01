4fb523370264ae6f1e35a605b5692b5fcc20300c
halil37 <hllibrahimsartitas@gmail.com>
2024-04-01 18:00:17 +0300

    BinarySearch project created and added in solution.

---
+++ src/Algorithm.Search.Binary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Algorithm.Search.Binary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
