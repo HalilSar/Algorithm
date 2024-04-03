f5638188eb28f587324a548da3e92f9a441e21ec
halil37 <hllibrahimsartitas@gmail.com>
2024-04-03 23:50:03 +0300

    program.cs modified in hashtable project.

--- src/Algorithm.Search.HashTableAlgorithm/Program.cs
+++ src/Algorithm.Search.HashTableAlgorithm/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using static System.Console;
+using System.Collections;
 
 namespace Algorithm.Search.HashTableAlgorithm
 {
@@ -6,7 +7,19 @@ namespace Algorithm.Search.HashTableAlgorithm
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Hashtable hashtable = new Hashtable();
+
+            hashtable.Add("key1", "value1");
+            hashtable.Add("key2", "value2");
+            hashtable.Add("key3", "value3");
+            hashtable.Add("key4", "value4");
+
+            string key = "key3";
+
+            if (hashtable.ContainsKey(key)) WriteLine($"key.: {key}  value: {hashtable[key]}");
+
+            else WriteLine("key not found.");
+
         }
     }
 }
