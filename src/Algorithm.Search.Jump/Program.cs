4ac647a9ef0fc5af68742ed662fef932a262c8b1
halil37 <hllibrahimsartitas@gmail.com>
2024-04-04 20:50:22 +0300

    jumpsearch project added in solution.

---
+++ src/Algorithm.Search.Jump/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Algorithm.Search.Jump
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+        //static int JumpSearch(int[] arr, int target)
+        //{
+           
+        //}
+    }
+}
