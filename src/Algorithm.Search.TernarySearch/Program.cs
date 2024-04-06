c069dcbd43511a3991cd72d13606e47c7a568939
halil37 <hllibrahimsartitas@gmail.com>
2024-04-06 21:12:22 +0300

    ternary project added to solution.

---
+++ src/Algorithm.Search.TernarySearch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Algorithm.Search.TernarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
