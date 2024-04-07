f50a8c9ea3747b087d4311dd9777138d411b0493
halil37 <hllibrahimsartitas@gmail.com>
2024-04-07 20:36:44 +0300

    Binary Search Tree project created.

---
+++ src/Algorithm.Search.BinarySearchTree/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Algorithm.Search.BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
