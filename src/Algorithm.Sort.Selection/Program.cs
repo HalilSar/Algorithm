614d9c1d1425e4c17a98dbb4eeeb87979f89add3
halil37 <hllibrahimsartitas@gmail.com>
2024-04-10 23:59:00 +0300

    programcs modified in selection project.

--- src/Algorithm.Sort.Selection/Program.cs
+++ src/Algorithm.Sort.Selection/Program.cs
@@ -1,12 +1,47 @@
-﻿using System;
-
-namespace Algorithm.Sort.Selection
+﻿using static System.Console;
+namespace Algorithm.Sort.Quick
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] array = { 64, 34, 25, 12, 22, 11, 90 };
+            WriteLine("Orjinal Array:");
+            PrintArray(array);
+            Sort(array, 0, array.Length - 1);
+            WriteLine("\n Sorte Array:");
+            PrintArray(array);
+        }
+
+
+        private static void Sort(int[] array, int left, int right)
+        {
+            if (left < right)
+            {
+                //int pivotIndex = Partition(array, left, right);
+                Sort(array, left, pivotIndex - 1);
+                Sort(array, pivotIndex + 1, right);
+
+            }
+        }
+        private static void Swap(int[] array, int i, int j)
+        {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+
+        }
+
+
+
+        private static void PrintArray(int[] array)
+        {
+            foreach (var item in array)
+            {
+                Write(item + " ");
+            }
+            WriteLine();
         }
     }
+}
 }
