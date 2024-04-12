f0efb425175734ca325493873dda9f4ec873cbeb
halil37 <hllibrahimsartitas@gmail.com>
2024-04-12 23:22:22 +0300

    shell project in solution.

---
+++ src/Algorithm.Sort.Shell/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Algorithm.Sort.Shell
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
