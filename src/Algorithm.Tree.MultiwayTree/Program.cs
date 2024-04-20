a9c4e5a11eed920a73dbafafe9c94608627da2db
halil37 <hllibrahimsartitas@gmail.com>
2024-04-20 22:13:59 +0300

    tree/multiwaytree created.

---
+++ src/Algorithm.Tree.MultiwayTree/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Algorithm.Tree.MultiwayTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
