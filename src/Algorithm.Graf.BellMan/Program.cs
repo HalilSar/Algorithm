d182a1bf186b86ac77c0f7ec0d5cf587c704656d
halil37 <hllibrahimsartitas@gmail.com>
2024-04-24 20:39:03 +0300

    BellMan project created.

---
+++ src/Algorithm.Graf.BellMan/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Algorithm.Graf.BellMan
+{
+    class Program
+    {
+        private int V;
+        private List<Tuple<int, int>>[] adj;
+
+        public Program(int v)
+        {
+            V = v;
+            adj = new List<Tuple<int, int>>[V];
+            for (int i = 0; i < V; ++i)
+            {
+                adj[i] = new List<Tuple<int, int>>();
+            }
+        }
+
+        public void AddEdge(int u, int v, int weight)
+        {
+            adj[u].Add(new Tuple<int, int>(v, weight));
+        }
+        static void Main(string[] args)
+        {
+            Program graph = new Program(5);
+            graph.AddEdge(0, 1, 10);
+            graph.AddEdge(0, 2, 5);
+            graph.AddEdge(1, 2, 2);
+            graph.AddEdge(1, 3, 1);
+            graph.AddEdge(2, 1, 3);
+            graph.AddEdge(2, 3, 9);
+            graph.AddEdge(2, 4, 2);
+            graph.AddEdge(3, 4, 4);
+            graph.AddEdge(4, 0, 7);
+            graph.AddEdge(4, 3, 6);
+        }
+    }
+}
