efd403d2b0c69ee752af17386ec39284f10675d8
halil37 <hllibrahimsartitas@gmail.com>
2024-04-25 21:44:26 +0300

    PrimMst created.

---
+++ src/Algorithm.Graf.PrimMST/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Algorithm.Graf.PrimMST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
