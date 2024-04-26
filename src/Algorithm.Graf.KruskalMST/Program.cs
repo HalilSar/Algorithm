815f30c4f4090bf17cad47f951f532c084832842
halil37 <hllibrahimsartitas@gmail.com>
2024-04-26 22:20:21 +0300

    KruskalMST created

---
+++ src/Algorithm.Graf.KruskalMST/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Algorithm.Graf.KruskalMST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
