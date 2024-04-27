37bbe366ae51740c6fd10074e346f785e9b37f6b
halil37 <hllibrahimsartitas@gmail.com>
2024-04-27 21:09:18 +0300

    Graf/Johnson project add to solution.

---
+++ src/Algorithm.Graf.Johnson/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Algorithm.Graf.Johnson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
