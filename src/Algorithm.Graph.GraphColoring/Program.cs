d4d400aef3e5d1bcfec250068bec9621c921181e
halil37 <hllibrahimsartitas@gmail.com>
2024-04-30 23:42:33 +0300

    GraphColoring added to solution.

---
+++ src/Algorithm.Graph.GraphColoring/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Algorithm.Graph.GraphColoring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
