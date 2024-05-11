92f91369f44e68dfff5c9824deb8e72dfd907906
halil37 <hllibrahimsartitas@gmail.com>
2024-05-11 22:41:30 +0300

    longestpathproblem added to solution.

---
+++ src/Algorithm.Graphic.LongestPathProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Algorithm.Graphic.LongestPathProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
