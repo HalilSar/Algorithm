d6aecdb1963855cba26375acfd0d18bcd69ca5e0
halil37 <hllibrahimsartitas@gmail.com>
2024-05-13 15:44:59 +0300

    MinimumSpanningTree project created.

---
+++ src/Algorithm.Graphic.MinimumSpanningTree/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Algorithm.Graphic.MinimumSpanningTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
